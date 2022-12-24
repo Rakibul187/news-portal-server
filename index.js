41337263f3b8a7f47edd83ab8874fef3b5b27550
rakib <rakibul241221@gmail.com>
2022-12-24 17:12:51 +0600

    category & specific news api

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.get('/', (req, res) => {
     res.send('News portal server is running')
 })
 
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '08') {
+        res.send(news)
+    }
+    else {
+        const selectedNews = news.filter(n => n.category_id == id)
+        res.send(selectedNews)
+    }
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    const selectedNews = news.find(n => n._id == id)
+    res.send(selectedNews)
+})
+
 app.get('/news-categories', (req, res) => {
     res.send(categories)
 })
